588a6de67023431df8b0525a790c914b37762d41
EmineYata <eminyata4@gmail.com>
2026-06-09 14:49:58 +0000

    first commit

--- src/main/java/mr/gov/finances/sgci/config/CorsAllowedOrigins.java
+++ src/main/java/mr/gov/finances/sgci/config/CorsAllowedOrigins.java
@@ -22,7 +22,8 @@ public final class CorsAllowedOrigins {
             "http://178.128.171.174:8081",
             "http://10.16.0.5:8081",
             "http://10.106.0.2:8081",
-            "http://172.18.0.1:8081"
+            "http://172.18.0.1:8081",
+            "http://10.1.230.60:8081"
     );
 
     public static boolean matchesFrontendOrigin(String originTrimmed) {
